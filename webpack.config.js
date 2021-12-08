76fcca3e2394b03b6df2d1892c3db715db866ecf
Will Darkins <willdarkins@gmail.com>
2021-12-08 00:29:03 -0800

    created webpack.config.js file and created main configuration to run webpack... also listed dependencies in package.json

--- webpack.config.js
+++ webpack.config.js
@@ -1,1 +1,14 @@
 const path = require("path");
+
+//create the main configuration object within our file to write options within this object that tell webpack what to do
+module.exports = {
+    //the entry point is the root of the bundle and the beginning of the dependency graph
+    entry: './assets/js/script.js',
+    //outputted bundled code sent to newly created dist directory
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'main.bundle.js'
+    },
+    //provide the mode in which we want webpack to run
+    mode: 'development'
+}
