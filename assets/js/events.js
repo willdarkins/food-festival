0e5b0857abff5b2c7c7aeabc02d25f7cb9597558
Will Darkins <willdarkins@gmail.com>
2021-12-08 21:28:36 -0800

    removed conditional page checker statements in tickets.js events.js and schedule.js in favor of the DOM ready method

--- assets/js/events.js
+++ assets/js/events.js
@@ -1,5 +1,11 @@
-//separating conditional statement from script.js realted to DOM created events 
-if (window.location.href.indexOf("event") > -1) {
+//separating conditional statement from script.js realted to DOM created events
+// DOM manipulation code specific to each page
+/*Because the DOM manipulation will now be linked to each HTML page directly through bundles,
+we should remove the page checker conditional from each of these three JavaScript files and
+replace it with the DOM ready method, which will then wrap the DOM manipulation just as
+the conditional statements had done*/
+
+$(document).ready(function () {
     const currentEvent = JSON.parse(localStorage.getItem("currentEvent")) || {
         title: "Title Placeholder",
         subtitle: "",
@@ -7,20 +13,18 @@ if (window.location.href.indexOf("event") > -1) {
     };
 
     const pageEl = document.querySelector("#page");
-    
-    const containerEl = createEl("div", {class: "container"},
-      createEl("div", {class: "card mb-3"}, 
-        createEl("img", {class: "card-img-top", src: currentEvent.image || "https://via.placeholder.com/350x150"}),
-        createEl("div", {class: "card-body"}, 
-          createEl("h1", {class: "card-title"}, currentEvent.title || ""),
-          createEl("h2", {class: "text-muted"}, currentEvent.subtitle || ""),
-          createEl("p", {class: "card-text mt-3"}, currentEvent.description || createLoremIpsum(100)),
-          createEl("a", {class: "btn btn-primary", href: "tickets.html"}, "Buy Tickets")
-        )
-      ),
-      
-    )
-    
 
+    const containerEl = createEl("div", { class: "container" },
+        createEl("div", { class: "card mb-3" },
+            createEl("img", { class: "card-img-top", src: currentEvent.image || "https://via.placeholder.com/350x150" }),
+            createEl("div", { class: "card-body" },
+                createEl("h1", { class: "card-title" }, currentEvent.title || ""),
+                createEl("h2", { class: "text-muted" }, currentEvent.subtitle || ""),
+                createEl("p", { class: "card-text mt-3" }, currentEvent.description || createLoremIpsum(100)),
+                createEl("a", { class: "btn btn-primary", href: "tickets.html" }, "Buy Tickets")
+            )
+        ),
+
+    )
     pageEl.appendChild(containerEl)
-  }
+})
--- assets/js/tickets.js
+++ assets/js/tickets.js
@@ -1,17 +1,22 @@
-//separating conditional statement from script.js realted to DOM created events 
-if (window.location.href.indexOf("tickets") > -1) {
+//separating conditional statement from script.js realted to DOM created events
+// DOM manipulation code specific to each page
+/*Because the DOM manipulation will now be linked to each HTML page directly through bundles,
+we should remove the page checker conditional from each of these three JavaScript files and
+replace it with the DOM ready method, which will then wrap the DOM manipulation just as
+the conditional statements had done*/
 
-    function purchaseTicket () {
+$(document).ready(function () {
+    function purchaseTicket() {
 
-      modalEl.removeChild(modalBodyEl)
-      modalEl.removeChild(modalFooterEl)
+        modalEl.removeChild(modalBodyEl)
+        modalEl.removeChild(modalFooterEl)
 
-      modalEl.append(createEl("div", {class: "modal-body"},
-        createEl("h5", {class: "modal-title"}, 
-        `Thanks for requesting a ticket purchase! We will send an email to ${purchaseEmail.value} to complete the order form!`
-        ),
-      ))
-      
+        modalEl.append(createEl("div", { class: "modal-body" },
+            createEl("h5", { class: "modal-title" },
+                `Thanks for requesting a ticket purchase! We will send an email to ${purchaseEmail.value} to complete the order form!`
+            ),
+        ))
+
     }
     purchaseBtn.addEventListener("click", purchaseTicket);
-  }
+})
