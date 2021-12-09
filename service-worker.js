0c9aab48c0030ec6dd10fdd5df126671cdab4b6a
Will Darkins <willdarkins@gmail.com>
2021-12-09 00:14:40 -0800

    added files to cache in service-workers.js file

---
+++ service-worker.js
@@ -0,0 +1,21 @@
+/*Before we add code to the callback function,
+we need to define which files we'd like to cache
+to the top of the service-worker.js file*/
+
+const FILES_TO_CACHE = [
+    "./index.html",
+    "./events.html",
+    "./tickets.html",
+    "./schedule.html",
+    "./assets/css/style.css",
+    "./assets/css/bootstrap.css",
+    "./assets/css/tickets.css",
+    "./dist/app.bundle.js",
+    "./dist/events.bundle.js",
+    "./dist/tickets.bundle.js",
+    "./dist/schedule.bundle.js"
+  ];
+
+self.addEventListener('install', function (e) {
+
+})
