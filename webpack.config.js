2541b7575ec10a4909ce120c4584a5f8c79c3a37
Will Darkins <willdarkins@gmail.com>
2021-12-08 22:52:45 -0800

    entry points for events schedule and tickets added to webpack.config.js while output name attribute has been left open

--- webpack.config.js
+++ webpack.config.js
@@ -5,26 +5,31 @@ const path = require("path");
 
 //create the main configuration object within our file to write options within this object that tell webpack what to do
 module.exports = {
-    //the entry point is the root of the bundle and the beginning of the dependency graph
-    entry: './assets/js/script.js',
-    //outputted bundled code sent to newly created dist directory
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'main.bundle.js'
-    },
-    //plugins play an important role in directing webpack what to do
-    plugins:[
-      //plug-in property to support jquery
-        new webpack.ProvidePlugin({
-          $: "jquery",
-          jQuery: "jquery"
-        }),
-        //plug-in property to support Bundle Analyzer
-        new BundleAnalyzerPlugin({
-          // the report outputs to an HTML file in the dist folder and loads on 'npm run build' command
-          analyzerMode: "static",
-        })
-      ],
-    //provide the mode in which we want webpack to run
-    mode: 'development'
+  //the entry point is the root of the bundle and the beginning of the dependency graph
+  entry: {
+    app: './assets/js/script.js',
+    events: "./assets/js/events.js",
+    schedule: "./assets/js/schedule.js",
+    tickets: "./assets/js/tickets.js"
+  },
+  //outputted bundled code sent to newly created dist directory
+  output: {
+    filename: "[name].bundle.js",
+    path: __dirname + "/dist",
+  },
+  //plugins play an important role in directing webpack what to do
+  plugins: [
+    //plug-in property to support jquery
+    new webpack.ProvidePlugin({
+      $: "jquery",
+      jQuery: "jquery"
+    }),
+    //plug-in property to support Bundle Analyzer
+    new BundleAnalyzerPlugin({
+      // the report outputs to an HTML file in the dist folder and loads on 'npm run build' command
+      analyzerMode: "static",
+    })
+  ],
+  //provide the mode in which we want webpack to run
+  mode: 'development'
 }
