dcef535bb193e7a6549e6c1434444f68c44253b2
Will Darkins <willdarkins@gmail.com>
2021-12-08 21:15:48 -0800

    created events.js to split js code based on responsibility for different page loads

---
+++ assets/js/events.js
@@ -0,0 +1,23 @@
+if (window.location.href.indexOf("index") > -1) {
+
+    carouselSlides.forEach((slide, i) => {
+      $('.carousel-inner').append(`
+    <div class="carousel-item fullscreen-carousel" style="background-image: url('${slide.img}')">
+      <div class="d-flex h-100 align-items-center justify-content-center carousel-caption">
+          <div class="container">
+            <div class="row align-items-center justify-content-center">
+                <h2 class="display-4 mb-2">${slide.title}</h2>
+            </div>
+            <div class="row align-items-center justify-content-center"> 
+              <h3>${slide.subtitle}</h3>
+            </div>
+            <div class=" mt-4 row align-items-center justify-content-center"> 
+              <a class="btn btn-primary" href="${slide.btnUrl}">
+                  ${slide.btnText}
+              </a>
+            </div>
+          </div>
+      </div>
+    </div>`)
+    })
+  }
--- assets/js/script.js
+++ assets/js/script.js
@@ -70,30 +70,6 @@ $(document).ready(function() {
     return result;
   }
 
-  if (window.location.href.indexOf("index") > -1) {
-
-    carouselSlides.forEach((slide, i) => {
-      $('.carousel-inner').append(`
-    <div class="carousel-item fullscreen-carousel" style="background-image: url('${slide.img}')">
-      <div class="d-flex h-100 align-items-center justify-content-center carousel-caption">
-          <div class="container">
-            <div class="row align-items-center justify-content-center">
-                <h2 class="display-4 mb-2">${slide.title}</h2>
-            </div>
-            <div class="row align-items-center justify-content-center"> 
-              <h3>${slide.subtitle}</h3>
-            </div>
-            <div class=" mt-4 row align-items-center justify-content-center"> 
-              <a class="btn btn-primary" href="${slide.btnUrl}">
-                  ${slide.btnText}
-              </a>
-            </div>
-          </div>
-      </div>
-    </div>`)
-    })
-  }
-
   if (window.location.href.indexOf("event") > -1) {
     const currentEvent = JSON.parse(localStorage.getItem("currentEvent")) || {
         title: "Title Placeholder",
