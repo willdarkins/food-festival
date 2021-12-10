36b4df9d01b8fa3b94af78082c8d17d3c4819d2b
Will Darkins <willdarkins@gmail.com>
2021-12-09 23:06:18 -0800

    got service worker setup

--- service-worker.js
+++ service-worker.js
@@ -21,6 +21,7 @@ const FILES_TO_CACHE = [
     "./dist/schedule.bundle.js"
 ];
 
+// Install the service worker
 self.addEventListener('install', function (e) {
     //e.waitUntil tells the browser to wait until the work is complete before terminating the service worker
     e.waitUntil(
@@ -28,6 +29,48 @@ self.addEventListener('install', function (e) {
         caches.open(CACHE_NAME).then(function (cache) {
             console.log('installing cache : ' + CACHE_NAME)
             return cache.addAll(FILES_TO_CACHE)
+
         })
     )
 })
+
+// Activate the service worker and remove old data from the cache
+self.addEventListener('activate', function(e) {
+    e.waitUntil(
+      caches.keys().then(function(keyList) {
+        let cacheKeeplist = keyList.filter(function(key) {
+          return key.indexOf(APP_PREFIX);
+        });
+        cacheKeeplist.push(CACHE_NAME);
+  
+        return Promise.all(
+          keyList.map(function(key, i) {
+            if (cacheKeeplist.indexOf(key) === -1) {
+              console.log('deleting cache : ' + keyList[i]);
+              return caches.delete(keyList[i]);
+            }
+          })
+        );
+      })
+    );
+  });
+
+// Intercept fetch requests
+
+self.addEventListener('fetch', function (e) {
+    console.log('fetch request : ' + e.request.url)
+    e.respondWith(
+      caches.match(e.request).then(function (request) {
+        if (request) { // if cache is available, respond with cache
+          console.log('responding with cache : ' + e.request.url)
+          return request
+        } else {       // if there are no cache, try fetching request
+          console.log('file is not cached, fetching : ' + e.request.url)
+          return fetch(e.request)
+        }
+  
+        // You can omit if/else for console.log & put one line below like this too.
+        // return request || fetch(e.request)
+      })
+    )
+  })
